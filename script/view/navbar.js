985b7bf30a53b5f15e964ca02fea61a0966e0a73
ariafatah0711 <ariafatah0711@gmail.com>
2023-12-11 14:58:57 +0700

    update perform website

--- script/view/navbar.js
+++ script/view/navbar.js
@@ -9,3 +9,28 @@ const showNavbar = () => {
 aAll.forEach((a) => {
   a.addEventListener("click", () => showNavbar());
 });
+
+/* scroll smoth navbar */
+// Check if the hash exists in the URL and navigate smoothly to the target
+document.addEventListener("DOMContentLoaded", function () {
+  const targetElement = document.querySelector(window.location.hash);
+  if (targetElement) {
+    targetElement.scrollIntoView({
+      behavior: "smooth",
+    });
+  }
+});
+
+// Add smooth scrolling for internal links
+document.addEventListener("click", function (event) {
+  const target = event.target;
+  if (target.tagName === "A" && target.getAttribute("href").startsWith("#")) {
+    const targetElement = document.querySelector(target.getAttribute("href"));
+    if (targetElement) {
+      event.preventDefault();
+      targetElement.scrollIntoView({
+        behavior: "smooth",
+      });
+    }
+  }
+});
